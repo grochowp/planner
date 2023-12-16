53bd0092d8c90753e679bd23115781e0f4d1c453
grochowp <patgro3@wp.pl>
2023-12-16 22:43:45 +0100

    add form styles

--- client/src/Routes/kanban/SelectTask.js
+++ client/src/Routes/kanban/SelectTask.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { User } from "./components/User";
 import { CSSTransition } from "react-transition-group";
 import "../../styles/FormAnimation.css";
+import Button from "../../shared/components/Button";
 
 export const SelectTask = ({ activeUser }) => {
   const [showForm, setShowForm] = useState(false);
@@ -53,13 +54,52 @@ export const SelectTask = ({ activeUser }) => {
         classNames="fade"
         unmountOnExit
       >
-        <div className="add-task-form" onClick={() => handleShowForm(false)}>
+        <div className="add-task-form">
           <div className="form">
             <form>
-              <input></input>
-              <input></input>
-              <input></input>
-              <input></input>
+              <div className="task-card-data add-task-top">
+                <p className="add-task-name">Add a new task!</p>
+                <p
+                  className="add-task-close"
+                  onClick={() => handleShowForm(false)}
+                >
+                  x
+                </p>
+              </div>
+              <div className="main-task-container">
+                <div className="main-task">
+                  <h5>New task</h5>
+                  <textarea
+                    className="main-task-input"
+                    placeholder="New task..."
+                  ></textarea>
+                  <div className="main-task-tasks">
+                    <div>
+                      <h5>Backlog</h5>
+                      <textarea placeholder="Backlog..."></textarea>
+                    </div>
+                    <div>
+                      <h5>To do</h5>
+                      <textarea placeholder="To do..."></textarea>
+                    </div>
+                    <div>
+                      <h5>Doing</h5>
+                      <textarea placeholder="Doing..."></textarea>
+                    </div>
+                    <div>
+                      <h5>Done</h5>
+                      <textarea placeholder="Done..."></textarea>
+                    </div>
+                  </div>
+                </div>
+                <div className="main-task-description">
+                  <h5>Description</h5>
+                  <textarea placeholder="Description..."></textarea>
+                </div>
+              </div>
+              <div className="add-task-button">
+                <Button styles="add-task">ADD TASK</Button>
+              </div>
             </form>
           </div>
         </div>
